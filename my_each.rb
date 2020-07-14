bb945b2257aeb8585cc60aafe2c4388746a2181b
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-14 02:51:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(list)
+  
+  counter = 0 
+  
+  while counter < list.length
+    yield (list[counter])
+    counter += 1 
+  end
+  
+  list
+  
 end
+ 
+  # collection = [1, 2, 3, 4]
+
+  # my_each(collection) do |i|
+  #   puts i 
+  # end
+  
